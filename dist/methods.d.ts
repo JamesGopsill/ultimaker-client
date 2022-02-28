0f9abb9fb8e50fe5601197f0ef3987c5e5d19cf1
James Gopsill <james.gopsill@cfms.org.uk>
2022-02-28 14:54:50 +0000

    updating the docs

--- dist/methods.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const getMethods: {
-    name: string;
-    endpoint: string;
-    expects: string;
-}[];
-export declare const getObjectMethods: {
-    name: string;
-    endpoint: string;
-}[];
