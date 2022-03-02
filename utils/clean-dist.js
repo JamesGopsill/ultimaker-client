cc64c4a69daf9be691f399e80bb4e3840a064484
James Gopsill <james.gopsill@gmail.com>
2022-03-02 08:43:21 +0000

    Further updates to dist and codebase. No Functionality changes.

--- utils/clean-dist.js
+++ utils/clean-dist.js
@@ -4,7 +4,9 @@ const dir = "./dist"
 // Clear the dist directory
 if (fs.existsSync(dir)) {
 	fs.rmSync(dir, { recursive: true })
-	console.log("Directory Cleaned!"); 
+	console.log("Directory Cleaned!")
+	fs.mkdirSync("./dist")
+	console.log("Folder Created")
 } else {
 	console.log("./dist does not exist")
 }
